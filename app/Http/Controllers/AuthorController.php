45ff82153e726230968fba944249f055552e690a
iqlimafasha <iqlimafasha27072003@gmail.com>
2025-05-21 22:36:39 +0700

    Read and Create Genre & Author API

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Author;
 
 class AuthorController extends Controller
 {
-    public function index() {
-        $data = new Author();
-        $authors = $data->getAuthors();
+    // READ all authors
+    public function index()
+    {
+        $authorModel = new Author();
+        $authors = $authorModel->getAuthors();
 
         return response()->json([
             "success" => true,
-            "message" => "Get all resources",
+            "message" => "Get all authors",
             "data" => $authors
         ], 200);
+    }
+
+    // CREATE a new author (simulasi)
+    public function store(Request $request)
+    {
+        $newAuthor = [
+            'id' => rand(100, 999), // simulasi ID
+            'name' => $request->name
+        ];
+
+        return response()->json([
+            "success" => true,
+            "message" => "Author created successfully",
+            "data" => $newAuthor
+        ], 201);
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Genre;
 use Illuminate\Http\Request;
+use App\Models\Genre;
 
 class GenreController extends Controller
 {
-    public function index() {
-        $genres = Genre::all();
+    // Read All Genres
+    public function index()
+    {
+        $genreModel = new Genre();
+        $genres = $genreModel->getGenres();
 
         return response()->json([
             "success" => true,
-            "message" => "Get all resources",
+            "message" => "Get all genres",
             "data" => $genres
         ], 200);
+    }
+
+    // Create Genre (simulasi)
+    public function store(Request $request)
+    {
+        $newGenre = [
+            'id' => rand(100, 999),
+            'name' => $request->name
+        ];
+
+        return response()->json([
+            "success" => true,
+            "message" => "Genre created successfully",
+            "data" => $newGenre
+        ], 201);
     }
 }
